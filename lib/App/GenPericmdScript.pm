50674f23eb8424d733973a5da77e4e5033048461
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-02-28 21:35:24 +0700

    Initial commit (not yet implemented)

---
+++ lib/App/GenPericmdScript.pm
@@ -0,0 +1,24 @@
+package App::GenPericmdScript;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+#use Log::Any '$log';
+
+our %SPEC;
+
+$SPEC{gen_perinci_cmdline_script} = {
+    v => 1.1,
+    summary => 'Generate Perinci::CmdLine CLI script',
+    args => {
+    },
+};
+sub gen_perinci_cmdline_script {
+    my %args = @_;
+}
+
+1;
+# ABSTRACT:
